7cd3a72de2ba49aa931d4d172f4c4d131335d998
nonnycode <miknonny@gmail.com>
2016-01-18 16:34:16 +0100

    changed path.resolve to path.join in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,8 @@ var webpack = require('webpack');
 var merge = require('webpack-merge');
 
 var TARGET = process.env.npm_lifecycle_event;
-var ROOT_PATH = path.resolve(__dirname);
-var APP_PATH = path.resolve(ROOT_PATH, 'src');
-var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+var APP_PATH = path.join(__dirname, 'src');
+var BUILD_PATH = path.join(__dirname, 'build');
 
 var common = {
   entry: APP_PATH,
